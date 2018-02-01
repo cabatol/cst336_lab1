0cc58b01aec747056ac157532fdac2f0358351c1
Chino Abatol <cabatol@csumb.edu>
2018-02-01 01:45:27 +0000

    adding files

---
+++ Personal_Portfolio/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Personal Website: Chino Abatol </title>
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Lobster');
+        </style>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Chino Abatol </h1>
+        </header>
+        <nav>
+            <hr width="50%" />
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /><br />
+        
+        <main>
+            
+                <figure id="me">
+                    <img src="img/myPic.jpg" alt="Picture of me." />
+                </figure>
+                
+                <div id="welcomeText">
+                    Hello! <br/>
+                    <p>Thank you for visiting my professinal portfolio website.</p>
+                    <p>I am learning to be a Software Engineer at CSUMB.</p>
+                    <p>Feel free to contact me!</p>
+                    <br /> <br />
+                    
+                    <em>"No man can <strong>win</strong> every battle, but no man should fall without a <strong>struggle.</strong>"</em><br />
+                    - Peter Parker
+                </div>
+                
+        </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            
+            <hr width="50%">
+            CST336 - Internet Programming. 2018&copy; Abatol <br />
+            <p><strong>Disclaimer:</strong> The information in this webpage is used for academic purpose only.</p>
+            <p><img src="https://www.prepsportswear.com/media/images/college_logos/300x300/1749151_mktg_logo.png"/></p>
+
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
